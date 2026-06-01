afad1c5f0eda4188f8719431221d3d797d729bdd
Anorak2 <adamcoleberry@gmail.com>
2026-05-31 23:13:25 -0500

    added an easy way to set a random background from a playlist

---
+++ sway/wallpaper.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+FAVORITE="$HOME/Pictures/wallpapers/main-sky.jpeg"
+WALLPAPER_DIR="$HOME/Pictures/wallpapers"
+CUTOFF=30
+
+get_wallpaper() {
+  if [ $((RANDOM % 100)) -lt $CUTOFF ]; then
+    echo "$FAVORITE"
+  else
+    find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" \) | shuf -n1
+  fi
+}
+
+pkill swaybg
+[ "${1:-}" = "--startup" ] && sleep 2
+
+for output in eDP-1 HDMI-A-1; do
+  swaybg -o "$output" -i "$(get_wallpaper)" -m fill &
+  disown
+done
