23d615fe601d83eab9a1a95013b6feb37ebe7ac5
Anorak2 <adamcoleberry@gmail.com>
2026-06-25 20:31:59 -0500

    fix inconsistent sizing for CPU/Memory widgets leading to constant screen resizing

--- waybar/scripts/cpu_pie.sh
+++ waybar/scripts/cpu_pie.sh
@@ -2,16 +2,23 @@
 
 used_pct=$(top -bn1 | awk '/^%Cpu/ { printf "%d", $2 + $4 }')
 
-if [ "$used_pct" -lt 20 ]; then
-  pie="○"
-elif [ "$used_pct" -lt 40 ]; then
-  pie="◔"
-elif [ "$used_pct" -lt 60 ]; then
-  pie="◑"
-elif [ "$used_pct" -lt 80 ]; then
-  pie="◕"
+if [ "$used_pct" -lt 13 ]; then
+  pie="󰪞"
+elif [ "$used_pct" -lt 26 ]; then
+  pie="󰪟"
+elif [ "$used_pct" -lt 39 ]; then
+  pie="󰪠"
+elif [ "$used_pct" -lt 52 ]; then
+  pie="󰪡"
+elif [ "$used_pct" -lt 65 ]; then
+  pie="󰪢"
+elif [ "$used_pct" -lt 78 ]; then
+  pie="󰪣"
+elif [ "$used_pct" -lt 91 ]; then
+  pie="󰪤"
 else
-  pie="●"
+  pie="󰪥"
 fi
 
 printf '{"text":"%s %s%%","tooltip":"CPU usage: %s%%"}\n' "$pie" "$used_pct" "$used_pct"
+
--- waybar/scripts/memory_pie.sh
+++ waybar/scripts/memory_pie.sh
@@ -2,16 +2,22 @@
 
 used_pct=$(free | awk '/Mem:/ { printf "%d", ($3/$2)*100 }')
 
-if [ "$used_pct" -lt 20 ]; then
-  pie="○"
-elif [ "$used_pct" -lt 40 ]; then
-  pie="◔"
-elif [ "$used_pct" -lt 60 ]; then
-  pie="◑"
-elif [ "$used_pct" -lt 80 ]; then
-  pie="◕"
+if [ "$used_pct" -lt 13 ]; then
+  pie="󰪞"
+elif [ "$used_pct" -lt 26 ]; then
+  pie="󰪟"
+elif [ "$used_pct" -lt 39 ]; then
+  pie="󰪠"
+elif [ "$used_pct" -lt 52 ]; then
+  pie="󰪡"
+elif [ "$used_pct" -lt 65 ]; then
+  pie="󰪢"
+elif [ "$used_pct" -lt 78 ]; then
+  pie="󰪣"
+elif [ "$used_pct" -lt 91 ]; then
+  pie="󰪤"
 else
-  pie="●"
+  pie="󰪥"
 fi
 
 printf '{"text":"%s %s%%","tooltip":"RAM usage: %s%%"}\n' "$pie" "$used_pct" "$used_pct"
